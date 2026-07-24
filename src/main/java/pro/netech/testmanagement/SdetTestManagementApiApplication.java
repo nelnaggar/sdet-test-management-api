996b8573e2f8db87da05dc284b511d3c4e43e0b5
Nader El-Naggar <nader@elnaggar.net>
2026-07-24 17:02:09 +0200

    Initial Spring Boot project

---
+++ src/main/java/pro/netech/testmanagement/SdetTestManagementApiApplication.java
@@ -0,0 +1,13 @@
+package pro.netech.testmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdetTestManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdetTestManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/netech/testmanagement/SdetTestManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.netech.testmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdetTestManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
